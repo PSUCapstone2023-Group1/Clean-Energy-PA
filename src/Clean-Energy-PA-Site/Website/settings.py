0d943926f810f187a30f60ede3dac9ddc3011eda
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-05-25 15:05:34 -0400

    more updates -_-

--- src/Clean-Energy-PA-Site/Website/settings.py
+++ src/Clean-Energy-PA-Site/Website/settings.py
@@ -81,11 +81,11 @@ WSGI_APPLICATION = "Website.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": env("POSTGRES_NAME"),
-        "USER": env("POSTGRES_USER"),
-        "PASSWORD": env("POSTGRES_PASSWORD"),
-        "HOST": env("POSTGRES_HOST"),
-        "PORT": env("POSTGRES_PORT"),
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": env("DB_PORT"),
     }
 }
 
