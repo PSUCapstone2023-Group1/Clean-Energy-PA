0dd15514835b53ddd6a9b9adacac35394bf141df
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-05-25 15:49:54 -0400

    testing base test case

--- src/Clean-Energy-PA-Site/Database/tests.py
+++ src/Clean-Energy-PA-Site/Database/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+import models
 
-# Create your tests here.
+# Hello World Model Test
+class DjangoTest(TestCase):
+    def create_user(self):
+        self.user =  models.User(name="Billy Bob", zipcode=99999)
+
+    def test_user_attribute(self):
+        self.assertEqual(self.user.name, "Billy Bob")
+        self.assertEqual(self.user.zipcode, 99999)
