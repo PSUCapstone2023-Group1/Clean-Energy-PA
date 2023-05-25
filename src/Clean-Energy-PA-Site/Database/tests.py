6af5ad1fe622c365236d33cd46337a1d4721a04f
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-05-25 16:00:56 -0400

    back to basics

--- src/Clean-Energy-PA-Site/Database/tests.py
+++ src/Clean-Energy-PA-Site/Database/tests.py
@@ -1,11 +1,5 @@
 from django.test import TestCase
-from Database.models import User
 
-# Hello World Model Test
-class DjangoTest(TestCase):
-    def create_user(self):
-        self.user =  User(name="Billy Bob", zipcode=99999)
-
-    def test_user_attribute(self):
-        self.assertEqual(self.user.name, "Billy Bob")
-        self.assertEqual(self.user.zipcode, 99999)
+# Note to developers:
+# Tests against the Database will be written here. 
+# Suggestion: Basic Hello World Test
