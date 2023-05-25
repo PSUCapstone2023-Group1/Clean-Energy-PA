12cb134b2e89bf688500c3993385f1ebb4295784
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-05-25 14:48:05 -0400

    testing updating DB

--- src/Clean-Energy-PA-Site/Website/settings.py
+++ src/Clean-Energy-PA-Site/Website/settings.py
@@ -78,16 +78,16 @@ WSGI_APPLICATION = "Website.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql_psycopg2",
-#         "NAME": env("DB_NAME"),
-#         "USER": env("DB_USER"),
-#         "PASSWORD": env("DB_PASSWORD"),
-#         "HOST": env("DB_HOST"),
-#         "PORT": env("DB_PORT"),
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": env("POSTGRES_NAME"),
+        "USER": env("POSTGRES_USER"),
+        "PASSWORD": env("POSTGRES_PASSWORD"),
+        "HOST": env("POSTGRES_HOST"),
+        "PORT": env("POSTGRES_PORT"),
+    }
+}
 
 
 # Password validation
