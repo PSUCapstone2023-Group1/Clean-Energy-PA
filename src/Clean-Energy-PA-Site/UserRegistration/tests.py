4d27bde3c9a9fece31c2415e0865a177532080f8
gitSCSdotcom <kahan122922@gmail.com>
2023-05-29 08:05:37 -0700

    added redirect from registration to login, added test that signup link visible

--- src/Clean-Energy-PA-Site/UserRegistration/tests.py
+++ src/Clean-Energy-PA-Site/UserRegistration/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
 # Create your tests here.
+
+
+# Test that Signup link is visible
+class SignupLinkTest(TestCase):
+    def test_signup_link_visible(self):
+        response = self.client.get("/register/")
+        self.assertEqual(response.status_code, 200)
+
+        # Assert that the signup link is present in the rendered HTML
+        self.assertContains(response, '<a href="/register">Sign up</a>')
