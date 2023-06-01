134fb5005852a3820e972f62ee19a80762d5f7a7
gitSCSdotcom <kahan122922@gmail.com>
2023-05-31 20:58:40 -0700

    added a test to check that user can be added to the database

--- src/Clean-Energy-PA-Site/UserRegistration/tests.py
+++ src/Clean-Energy-PA-Site/UserRegistration/tests.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
 from django.urls import reverse
 from django.shortcuts import resolve_url
+from django.contrib.auth.models import User
+from UserRegistration.utils import generate_username, generate_email, generate_password
+
 
 # Create your tests here.
-
-
 # Test that Signup link is visible
 class SignupLinkTest(TestCase):
     def test_signup_link_visible(self):
@@ -24,11 +25,38 @@ class RegistrationTest(TestCase):
             data={
                 "username": "testuser",
                 "email": "testuser@test.com",
-                "password1": "password123",
-                "password2": "password123",
+                "password1": "password",
+                "password2": "password",
             },
         )
         self.assertEqual(response.status_code, 302)
         # Check if the response redirects to the login page
         expected_url = reverse("login")
         self.assertRedirects(response, expected_url, fetch_redirect_response=False)
+
+    def test_successful_registration_creates_account(self):
+        # Count the number of existing user accounts
+        existing_user_count = User.objects.count()
+        print(f"Existing User Count: {existing_user_count}")
+
+        # Generate a dummy password
+        password = generate_password()
+
+        # Simulate a POST request with valid registration data
+        response = self.client.post(
+            reverse("register"),
+            data={
+                "username": generate_username(),
+                "email": generate_email(),
+                "password1": password,
+                "password2": password,
+            },
+            follow=True,
+        )
+
+        # Check if the response status code is 200 (success)
+        self.assertEqual(response.status_code, 200)
+
+        # Check if a new user account is created
+        new_user_count = User.objects.count()
+        self.assertEqual(new_user_count, existing_user_count + 1)
---
+++ src/Clean-Energy-PA-Site/UserRegistration/utils.py
@@ -0,0 +1,24 @@
+import random
+import string
+
+
+def generate_username(length=8):
+    # Generate a random username with the given length
+    username = "".join(random.choices(string.ascii_lowercase + string.digits, k=length))
+    return username
+
+
+def generate_email(domain="example.com", length=8):
+    # Generate a random email address
+    username = generate_username(length)
+    email = f"{username}@{domain}"
+    return email
+
+
+def generate_password(length=12):
+    # Define a pool of allowed characters for the password
+    characters = string.ascii_letters + string.digits + string.punctuation
+
+    # Generate a random password with the given length
+    password = "".join(random.choice(characters) for _ in range(length))
+    return password
