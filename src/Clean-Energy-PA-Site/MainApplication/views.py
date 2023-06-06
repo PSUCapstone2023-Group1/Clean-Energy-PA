930783e0b452ea3ffaedb39f8c7c66ca4c25715e
gitSCSdotcom <kahan122922@gmail.com>
2023-06-05 18:49:49 -0700

    removed views.html and list.html from urls.py and views.py

--- src/Clean-Energy-PA-Site/MainApplication/views.py
+++ src/Clean-Energy-PA-Site/MainApplication/views.py
@@ -5,14 +5,5 @@ from .forms import RegisterUser
 
 
 # Create your views here.
-def index(response, id):
-    user = User_Preferences.objects.get(id=id)
-    return render(response, "MainApplication/list.html", {"user": user})
-
-
 def home(response):
     return render(response, "MainApplication/home.html", {})
-
-
-def view(response):
-    return render(response, "MainApplication/view.html", {})
