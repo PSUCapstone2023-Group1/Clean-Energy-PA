8ae211f7fb55bef7850fecce57d98caf11095db3
sam1010k <Kahan122922@gmail.com>
2023-06-24 14:11:11 -0700

    adding a selenium test for user profile

--- src/Clean-Energy-PA-Site/Website/settings.py
+++ src/Clean-Energy-PA-Site/Website/settings.py
@@ -81,7 +81,7 @@ ROOT_URLCONF = "Website.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, 'templates')],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
