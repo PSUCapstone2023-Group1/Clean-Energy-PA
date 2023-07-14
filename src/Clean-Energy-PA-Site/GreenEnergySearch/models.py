1472ff52c27305a1c50fcb0f76467e757dc18705
Keaton Foster <fosterkeaton@yahoo.com>
2023-07-13 22:30:25 -0600

    Added offer as JSON to the model

--- src/Clean-Energy-PA-Site/GreenEnergySearch/models.py
+++ src/Clean-Energy-PA-Site/GreenEnergySearch/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from web_parser.responses.ratesearch import offer
 
 
 # Create your models here.
@@ -8,16 +9,17 @@ class User_Preferences(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, default="default")
     # The users zip code
     zip_code = models.CharField(max_length=10, default="default")
-    # The rate schedule that the user saved
-    rate_schedule = models.CharField(max_length=80, default="default")
     # Distributor id from the api
     distributor_id = models.BigIntegerField(default=-1)
+    # The rate schedule that the user saved
+    rate_schedule = models.CharField(max_length=80, default="default")
     # Offer id from the api
-    selected_offer_id = models.BigIntegerField(default=-1)
-    # The price per kWh rate that the user saved
-    selected_offer_rate = models.FloatField(default=-1)
+    selected_offer = models.JSONField()
     # Users email notification preference
     email_notifications = models.BooleanField(default=True)
 
     def __str__(self):
         return str(self.user_id)
+    
+    def get_selected_offer(self)->offer:
+        return offer(self.selected_offer)
