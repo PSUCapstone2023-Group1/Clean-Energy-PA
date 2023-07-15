cafe360d1aab1f74af2ac451c673871197396064
sam1010k <Kahan122922@gmail.com>
2023-07-15 10:10:17 -0700

    get_subscribers, and build mailing list

--- src/Clean-Energy-PA-Site/price_watch/price_updater.py
+++ src/Clean-Energy-PA-Site/price_watch/price_updater.py
@@ -4,7 +4,8 @@ from price_watch import price_watchdog
 
 
 def start():
-    scheduler = BackgroundScheduler()
-    Price_Watch_Dog_Instance = price_watchdog.Price_Watch_Dog()
-    scheduler.add_job(Price_Watch_Dog_Instance.check_user_rates, "interval", seconds=5)
-    scheduler.start()
+    pass  # passing to avoid running in production while testing
+    # scheduler = BackgroundScheduler()
+    # Price_Watch_Dog_Instance = price_watchdog.Price_Watch_Dog()
+    # scheduler.add_job(Price_Watch_Dog_Instance.check_user_rates, "interval", seconds=5)
+    # scheduler.start()
--- src/Clean-Energy-PA-Site/price_watch/price_watchdog.py
+++ src/Clean-Energy-PA-Site/price_watch/price_watchdog.py
@@ -1,20 +1,24 @@
 from django.contrib.auth.models import User
 from GreenEnergySearch.models import User_Preferences
 from web_parser.papowerswitch_api import papowerswitch_api
-from web_parser.responses.ratesearch import price_structure
 import pandas as pd
 
 api = papowerswitch_api()
 
 
 class Price_Watch_Dog:
-    # build group {email_notifications, zip_code, rate_type}
-    # 2 tables key pair (distributor_id, rate_type)
-    # thread that compares based on groups found
-    # passing the model data from django to pricewatch
+    def remove_rows_with_zero(self, df, col):
+        """Keep rates that are not zero"""
+        df = df[df[col] != 0]
+        return df
+
+    def compare_rates(self, df, rate, lower_rate_threshold):
+        """Keep rates that are lower than threshold"""
+        df = df[df[rate] < df[lower_rate_threshold]]
+        return df
 
     def get_all_subscribers(self):
-        """Returns a dataframe of subscribers
+        """Returns a dataframe of all subscribers
         (folks who selected True for email notifications)"""
         users = User.objects.all()
         subscribed_users = []
@@ -23,51 +27,76 @@ class Price_Watch_Dog:
                 user_preferences = User_Preferences.objects.get(
                     user_id=user, email_notifications=True
                 )
+
+                user_offer = user_preferences.get_selected_offer()
+                # Create a list that will be converted to pandas DataFrame
                 subscribed_users.append(
                     {
-                        "user_id": user_preferences.user_id,
+                        "email": user.email,
                         "zip_code": user_preferences.zip_code,
                         "rate_schedule": user_preferences.rate_schedule,
                         "distributor_id": user_preferences.distributor_id,
-                        "selected_offer_id": user_preferences.selected_offer_id,
-                        "selected_offer_rate": user_preferences.selected_offer_rate,
+                        "selected_offer_rate": user_offer.rate,
                     }
                 )
             except User_Preferences.DoesNotExist:
                 pass
+        # Create the subscribers DataFrame
         subscribers_df = pd.DataFrame(subscribed_users)
         return subscribers_df
 
-    def get_unique_rate_search_params(self, df):
-        """returns a dataframe that represents all unique combinations"""
-        df = df.drop(columns=["user_id", "selected_offer_id"], axis=1)
-        df = df.reset_index(drop=True)
-        # Specify the columns you want to consider for uniqueness
-        columns_to_check = [
-            "zip_code",
-            "rate_schedule",
-            "distributor_id",
-            "selected_offer_rate",
-        ]
-        # Drop duplicates based on the specified columns
-        df = df.drop_duplicates(subset=columns_to_check)
-        print(df)
-        return df
+    def build_lower_rate_mailing_list_df(self, row):
+        # Zip Search on subcriber zip_code row
+        distributors = api.get_distributors(row["zip_code"])
+        # Temp list to hold appended data
+        data = []
+        # Get offers for each distributor and build the mailing list
+        for distributor in distributors:
+            # Rate Search on all available distributors
+            # **assuming subscribers existing rate_schedule**
+            offers = api.get_offers(distributor.id, row["rate_schedule"])
+            for offer in offers:
+                rate = offer.rate
+                # Build out the mailing list that will be converted to dataframe
+                data.append(
+                    {
+                        # Subscriber data (user data from db w/ email_notification=True)
+                        "email": row["email"],
+                        "zip_code": row["zip_code"],
+                        "rate_schedule": row["rate_schedule"],
+                        "existing_distributor_id": row["distributor_id"],
+                        "existing_offer_rate": row["selected_offer_rate"],
+                        # Returned from the Rate Search, lower rate data
+                        "lower_distributor_id": distributor.id,
+                        "lower_distributor_name": distributor.name,
+                        "lower_offer_name": offer.name,
+                        "lower_rate": rate,
+                    }
+                )
+        # Create the mailing_list DataFrame
+        lower_rate_mailing_list_df = pd.DataFrame(data)
+
+        # If Rate Search returns a rate of 0 remove row
+        lower_rate_mailing_list_df = self.remove_rows_with_zero(
+            lower_rate_mailing_list_df, "lower_rate"
+        )
+
+        # Remove any duplicates
+        lower_rate_mailing_list_df = lower_rate_mailing_list_df.drop_duplicates()
+
+        # Compare rates and only keep rates that are lower than users existing (threshold)
+        lower_rate_mailing_list_df = self.compare_rates(
+            lower_rate_mailing_list_df, "lower_rate", "existing_offer_rate"
+        )
+        return lower_rate_mailing_list_df
 
     def check_user_rates(self):
         subscribers_df = self.get_all_subscribers()
-        unique_rate_search_df = self.get_unique_rate_search_params(subscribers_df)
+        lower_rate_mailing_list_df = pd.DataFrame()  # Create an empty DataFrame
 
-        # for zip_code in zip_codes:
-        #     distributors = api.get_distributors(zip_code)
-        #     print(f"There are {len(distributors)} distributors")
-        #     for distributor in distributors:
-        #         print(f"zip_code: {zip_code}")
-        #         rates = distributor.rates
-        #         print(f"There are {len(rates)} rates")
-        #         for rate in rates:
-        #             print(f"Rate: {rate.id}")
-        #             offers = api.get_offers(rate.id, "R - Regular Residential Service")
-        #             print(f"There are {len(offers)} offers")
-        #             for offer in offers:
-        #                 print(f"Offer: {offer}")
+        # For each subscriber, append lower rates/offers
+        for index, row in subscribers_df.iterrows():
+            lower_rates_df = self.build_lower_rate_mailing_list_df(row)
+            lower_rate_mailing_list_df = pd.concat(
+                [lower_rate_mailing_list_df, lower_rates_df], ignore_index=True
+            )
