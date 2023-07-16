139c962dc1872b5aae61ad0f8367f1588ee08d70
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-07-16 16:06:50 -0400

    testing maybe needs to have location added

--- src/Clean-Energy-PA-Site/Website/wsgi.py
+++ src/Clean-Energy-PA-Site/Website/wsgi.py
@@ -8,8 +8,11 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
+
+sys.path.insert(0, '/opt/python/current/app')
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Website.settings")
 
