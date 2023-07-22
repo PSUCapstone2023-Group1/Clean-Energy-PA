3258b30c2edab9d20db2010301ff3564e30a7076
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-07-22 13:29:10 -0400

    trying to clean up

--- src/Clean-Energy-PA-Site/Website/wsgi.py
+++ src/Clean-Energy-PA-Site/Website/wsgi.py
@@ -8,8 +8,18 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
 from django.core.wsgi import get_wsgi_application
 
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+web_parser_path = os.path.abspath(os.path.join(path, "..", "web_parser")) 
+
+if path not in sys.path:     
+    sys.path.append(path) 
+    
+if web_parser_path not in sys.path:
+    sys.path.append(web_parser_path) 
+    
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Website.settings")
 
 application = get_wsgi_application()
--- src/Clean-Energy-PA-Site/__init__.py
+++ src/Clean-Energy-PA-Site/__init__.py
@@ -1,12 +1,11 @@
-print("Vlad Testing Source")
 import os 
+import sys 
 
 path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 web_parser_path = os.path.abspath(os.path.join(path, "web_parser")) 
-import sys 
-print("Vlad Testing Source", path)
-print("Vlad Testing Source", web_parser_path)
 
-sys.path.append(path)
-sys.path.append(web_parser_path)
-print("Printing sys path", sys.path)
+if path not in sys.path:     
+    sys.path.append(path) 
+    
+if web_parser_path not in sys.path:
+    sys.path.append(web_parser_path) 
