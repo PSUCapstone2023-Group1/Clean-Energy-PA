f37ccc9a4b57b6b22adf306b5421b975338a28c6
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-07-22 13:12:20 -0400

    updated init

--- src/Clean-Energy-PA-Site/__init__.py
+++ src/Clean-Energy-PA-Site/__init__.py
@@ -1,4 +1,12 @@
-import sys
-sys.path.append("..")
-sys.path.append("../web_parser")
-sys.path.append("../web_parser/tests")
+print("Vlad Testing Source")
+import os 
+
+path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+web_parser_path = os.path.abspath(os.path.join(path, "web_parser")) 
+import sys 
+print("Vlad Testing Source", path)
+print("Vlad Testing Source", web_parser_path)
+
+sys.path.append(path)
+sys.path.append(web_parser_path)
+print("Printing sys path", sys.path)
