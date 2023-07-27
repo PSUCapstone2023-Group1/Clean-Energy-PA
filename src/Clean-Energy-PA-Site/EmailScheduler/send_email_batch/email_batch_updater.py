73d0a529d97c4ae9faa492443b7c46bc6cabc0b9
sam1010k <Kahan122922@gmail.com>
2023-07-26 23:23:27 -0700

    fixing email updater to days not day

--- src/Clean-Energy-PA-Site/EmailScheduler/send_email_batch/email_batch_updater.py
+++ src/Clean-Energy-PA-Site/EmailScheduler/send_email_batch/email_batch_updater.py
@@ -18,7 +18,7 @@ def start():
         Email_Batch_Instance.send_contract_expiration_emails,
         id="send_contract_expiration_emails",
         trigger="interval",
-        day=1,
+        days=1,
     )
 
     scheduler.start()
