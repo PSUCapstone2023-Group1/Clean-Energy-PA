d8509e92afce7fbe3f18f8e0444c1ba37fe3ca73
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-08-04 17:10:42 -0400

    another errors with import in webparser not webapp

--- src/web_parser/__init__.py
+++ src/web_parser/__init__.py
@@ -1,7 +1,3 @@
-from .papowerswitch_api import papowerswitch_api
-from .responses.ratesearch import offer, offer_collection, price_structure
-from .responses.zipsearch import distributor, distributor_collection
-
 import sys
 sys.path.append('.')
 sys.path.append('./tests')
