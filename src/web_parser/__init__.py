1aab595f06d0c986d71ba6d1a20773fe4b830f53
Vladimir Tattybayev <vtattyba@buffalo.edu>
2023-08-04 17:32:33 -0400

    more path modifications for webparser

--- src/web_parser/__init__.py
+++ src/web_parser/__init__.py
@@ -1,3 +1,9 @@
 import sys
+import os 
 sys.path.append('.')
 sys.path.append('./tests')
+
+path = os.path.dirname(os.path.abspath(__file__))
+test_path = os.path.abspath(os.path.join(path, "test")) 
+sys.path.append(path)
+sys.path.append(test_path)
